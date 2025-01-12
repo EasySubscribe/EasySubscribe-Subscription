af22e21e8dc4a9af6ff66d63928fda66fa27f4a8
Giovanni <giovannilamarmora.working@gmail.com>
2025-01-12 18:46:31 +0100

    feat(): Empty Template

---
+++ dist/templates/template-empty.php
@@ -0,0 +1,31 @@
+<?php
+/* Template Name: Login Page */
+?>
+<html lang="it-IT" data-lt-installed="true">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="robots" content="noindex, nofollow" />
+    <meta name="googlebot" content="noindex, nofollow, noarchive" />
+
+    <link rel="shortcut icon" href="https://www.easysubscribe.it/wp-content/uploads/2025/01/easy.png" />
+    <meta
+      content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"
+      name="viewport"
+    />
+    <?php require __DIR__ . '/../inc/shared.php'; ?>
+    <link rel="stylesheet" href="../assets/css/contact-me.css" />
+    <script src="../assets/js/contact-me.js"></script>
+    <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+    />
+    <title>Contact - EasySubscribe</title>
+  </head>
+  <body>
+    <?php require __DIR__ . '/../inc/header.php'; ?>
+
+    
+    <?php require __DIR__ . '/../inc/footer.php'; ?>
+  </body>
+</html>
